2b8a8292cf74b7176e53bdfc743bd30cdf5e3404
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-09-24 01:10:13 +0700

    chore: render react stream

---
+++ pages/coins/[id]/page.js
@@ -0,0 +1,3 @@
+export default function Page(props) {
+    return <h1>OK</h1>;
+}
---
+++ src/entry.worker.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOMServer from "react-dom/server";
+function Root() {
+    return (<html>
+            <head>
+                <title>Hello world</title>
+            </head>
+            <body>
+                <header>This is header</header>
+                <main>This is main</main>
+                <footer>This is footer</footer>
+            </body>
+        </html>);
+}
+export default {
+    async fetch(request) {
+        const stream = await ReactDOMServer.renderToReadableStream(<Root />);
+        return new Response(stream);
+    },
+};
